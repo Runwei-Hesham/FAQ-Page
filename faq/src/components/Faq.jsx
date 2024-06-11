d04bee88e6ad9e5c6d64a89c1e38c8023a380a5d
Hesham <heshamsharafaldin@gmail.com>
2024-06-11 15:46:12 -0400

    FEAT: Major Improvements (Tex5, images, navbar, etc)

--- faq/src/components/Faq.jsx
+++ faq/src/components/Faq.jsx
@@ -1,4 +1,9 @@
 import "../css/faq.css";
+import logo from "../Gallery/Social media logos/image.png";
+import sipc from "../Gallery/SIPC/image.png";
+import fidelity from "../Gallery/companies/fidelity.png";
+import blackrock from "../Gallery/companies/BlackRock.png";
+import jpmorgan from "../Gallery/companies/Jpmorgan.png";
 function Faq() {
   return (
     <div className="items">
@@ -27,10 +32,167 @@ function Faq() {
       <div className="center">
         <h1>Check the fine print.</h1>
       </div>
+      <div className="center-rectangles">
+        <div className="rectangle">
+          <h2>Privacy Policies</h2>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry Privacy Policy</h4>
+            <p>
+              This discloses our privacy practices for personal information we
+              collect when you use or access our products, services, features,
+              or content.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry Privacy Policy</h4>
+            <p>
+              This discloses our privacy practices for personal information we
+              collect when you use or access our products, services, features,
+              or content.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry Privacy Policy</h4>
+            <p>
+              This discloses our privacy practices for personal information we
+              collect when you use or access our products, services, features,
+              or content.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+        </div>
+        <div className="rectangle">
+          <h2>Regulatory Compliance</h2>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry GLBA Notice 2</h4>
+            <p>
+              This explains our information-sharing practices and how we
+              safeguard sensitive data.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry GLBA Notice 2</h4>
+            <p>
+              This explains our information-sharing practices and how we
+              safeguard sensitive data.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry GLBA Notice 2</h4>
+            <p>
+              This explains our information-sharing practices and how we
+              safeguard sensitive data.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+        </div>
+        <div className="rectangle">
+          <h2>Terms and Conditions</h2>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry Terms of Service</h4>
+            <p>
+              These Account Terms of Service (“TOS”) are a legally binding
+              agreement between you, Trust Fund Registry and our financial
+              institution partners.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry Terms of Service</h4>
+            <p>
+              These Account Terms of Service (“TOS”) are a legally binding
+              agreement between you, Trust Fund Registry and our financial
+              institution partners.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+          <div className="white-rectangle">
+            <h4>Trust Fund Registry Terms of Service</h4>
+            <p>
+              These Account Terms of Service (“TOS”) are a legally binding
+              agreement between you, Trust Fund Registry and our financial
+              institution partners.
+            </p>
+            <button className="white-button">Download PDF</button>
+            <button className="purple-button">Read Article</button>
+          </div>
+        </div>
+      </div>
+      <div className="center margin1">
+        <h1>20,000+ families and children are investing with us.</h1>
+      </div>
+      <h4>We’ve partnered with the most trusted financial services. </h4>
+      
       <div className="center">
-        <div className="rectangle"></div>
-        <div className="rectangle"></div>
-        <div className="rectangle"></div>
+      <div className="company-images">
+          <img src={fidelity}></img>
+          <img src={blackrock}></img>
+          <img src={jpmorgan}></img>
+        </div>
+        <div className="center-column">
+          <p>Every account-holder is</p>
+          <img src={sipc} width="70" height=""></img>
+          <p>protected up to $250,000.</p>
+        </div>
+      </div>
+      <h4>More questions?</h4>
+      <div className="footer">
+        <div className="footer-items">
+          <ul>
+            <p>About us</p>
+            <p>Gifting</p>
+            <p>Help Center</p>
+          </ul>
+          <ul>
+            <h4>TrustFund Registry</h4>
+          </ul>
+          <ul>
+            <p>Education</p>
+            <p>Security</p>
+            <p>Contact Us</p>
+          </ul>
+        </div>
+        <div className="divider"></div>
+        <div className="center-column">
+          <div className="image-container">
+            <img src={logo} alt="Social media links"></img>
+          </div>
+          <p>© 2010 — 2023</p>
+          <p>Privacy — Terms</p>
+          <p>
+            TrustFundRegistry is the tradename of TrustFundRegistry LLC, an
+            SEC-registered investment adviser. Our internet-based investment
+            advisory services are designed to assist clients in saving for
+            college and are not intended to provide comprehensive tax advice or
+            financial planning. Our services are available to U.S. residents
+            only. This website shall not be considered a solicitation or
+            offering for any service or product to any person in any
+            jurisdiction where such solicitation or offer would be unlawful.
+          </p>
+          <p>
+            Investing in securities involves risks, and investing in 529 plans
+            in particular involves certain tax considerations and related risks.
+            Any investments you make in a 529 plan through TrustFundRegistry may
+            lose value. Historical returns, expected returns, and probability
+            projections are provided for informational and illustrative
+            purposes, and may not reflect actual future performance. Our savings
+            calculator and pricing comparison are hypothetical tools provided
+            for illustrative purposes only and are based on important
+            assumptions described here.
+          </p>
+        </div>
       </div>
     </div>
   );
